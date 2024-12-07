c8a6c605e97a8cd7fca4d1e5e03b5cdb0273e0bf
RuiCarvalho91 <ruydecarvalho91@gmail.com>
2024-12-07 15:31:16 +0000

    simple soundtrack

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 public class Main {
     public static void main(String[] args) {
 
+        PlayMusic playMusic = new PlayMusic();
+        playMusic.makeSound();
         Game game = new Game(4, 3, 100);
         game.start();
 
